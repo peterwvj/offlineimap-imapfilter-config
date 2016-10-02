9389504ab22abafd1b84850237dcab7c9ba90883
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-10-02 12:19:21 +0200

    Rename Python function for clarity

--- .offlineimap.py
+++ .offlineimap.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import re, os
 
-def get_password_emacs(machine, login, port):
+def get_password(machine, login, port):
     s = "machine %s login %s port %s password ([^ ]*)\n" % (machine, login, port)
     p = re.compile(s)
     authinfo = os.popen("gpg2 -q --no-tty -d ~/.authinfo.gpg").read()
